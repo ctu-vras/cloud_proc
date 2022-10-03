91f1baaf095f790e16c993d9081db402365410de
Tomas Petricek <tpetricek@gmail.com>
2022-10-03 15:23:12 +0200

    Allow rotation into given frame prior projection, allow to specify field of view.

--- include/cloud_proc/projection.h
+++ include/cloud_proc/projection.h
@@ -4,6 +4,7 @@
 #include <cloud_proc/common.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 namespace cloud_proc
 {
@@ -46,6 +47,8 @@ public:
 
   int height_ = 0;
   int width_ = 0;
+  T fov_azimuth_ = std::numeric_limits<T>::quiet_NaN();
+  T fov_elevation_ = std::numeric_limits<T>::quiet_NaN();
   T f_azimuth_ = std::numeric_limits<T>::quiet_NaN();
   T f_elevation_ = std::numeric_limits<T>::quiet_NaN();
   T c_azimuth_ = std::numeric_limits<T>::quiet_NaN();
@@ -53,15 +56,22 @@ public:
   int keep_ = KEEP_LAST;
   bool azimuth_only_ = false;
 
-  void process(const sensor_msgs::PointCloud2& input, sensor_msgs::PointCloud2& output)
+  void process(const sensor_msgs::PointCloud2& input,
+               const geometry_msgs::Transform& transform,
+               sensor_msgs::PointCloud2& output)
   {
+    typedef Eigen::Vector3f Vec3f;
+    typedef Eigen::Map<const Vec3f> ConstVec3fMap;
+    typedef Eigen::Transform<float, 3, Eigen::Isometry> Transform3f;
     assert(input.height >= 1);
     assert(input.width >= 1);
     assert(input.row_step == input.width * input.point_step);
+    // Construct eigen transform to target frame.
+    Transform3f tf = tf2::transformToEigen(transform).cast<float>();
     output.header = input.header;
-    output.height = (azimuth_only_ || height_ == 0) ? input.height : height_;
+    output.height = (azimuth_only_ || height_ <= 0) ? input.height : height_;
     assert(output.height >= 1);
-    output.width = (width_ == 0) ? input.width : width_;
+    output.width = (width_ <= 0) ? input.width : width_;
     assert(output.width >= 1);
     output.fields = input.fields;
     output.is_bigendian = input.is_bigendian;
@@ -72,8 +82,10 @@ public:
     output.data.resize(size_t(output.height) * output.row_step, 0);
     output.is_dense = input.is_dense;
 
-    T f_azimuth = (std::isfinite(f_azimuth_) && f_azimuth_ != 0) ? f_azimuth_ : -T(output.width) / (2 * M_PI);
-    T f_elevation = (std::isfinite(f_elevation_) && f_elevation_ != 0) ? f_elevation_ : -T(output.height) / (M_PI / 2);
+    T fov_azimuth = std::isfinite(fov_azimuth_) ? fov_azimuth_ : (2 * M_PI);
+    T fov_elevation = std::isfinite(fov_elevation_) ? fov_elevation_ : (M_PI_2);
+    T f_azimuth = std::isfinite(f_azimuth_) ? f_azimuth_ : -T(output.width) / fov_azimuth;
+    T f_elevation = std::isfinite(f_elevation_) ? f_elevation_ : -T(output.height) / fov_elevation;
     T c_azimuth = std::isfinite(c_azimuth_) ? c_azimuth_ : T(output.width) / 2 - 0.5;
     T c_elevation = std::isfinite(c_elevation_) ? c_elevation_ : T(output.height) / 2 - 0.5;
 
@@ -83,10 +95,12 @@ public:
     {
       for (size_t j_in = 0; j_in < input.width; ++j_in, ++x_in)
       {
-        if (!is_point_valid(x_in[0], x_in[1], x_in[2], false))
+        ConstVec3fMap p(&x_in[0]);
+        if (!is_point_valid(p(0), p(1), p(2), false))
           continue;
+        Vec3f q = (transform.rotation.w == 1) ? p : tf * p;
         T u, v;
-        u = f_azimuth * azimuth(x_in[0], x_in[1], x_in[2]) + c_azimuth;
+        u = f_azimuth * azimuth(q(0), q(1), q(2)) + c_azimuth;
         // Move [-0.5, 0.5) to [0, 1).
         u += 0.5;
         // Skip points outside the image.
@@ -98,23 +112,22 @@ public:
           i_out = i_in;
         else
         {
-          v = f_elevation * elevation(x_in[0], x_in[1], x_in[2]) + c_elevation;
+          v = f_elevation * elevation(q(0), q(1), q(2)) + c_elevation;
           v += 0.5;
           if (v < 0 || v >= output.height || std::isnan(v))
             continue;
           i_out = v;
         }
+        auto x_out = x_out_begin + i_out * output.width + j_out;
         // Skip if target pixel already contains a valid point.
         if (keep_ == KEEP_FIRST)
         {
-          auto x_out = x_out_begin + i_out * output.width + j_out;
           if (is_point_valid(x_out[0], x_out[1], x_out[2], false))
             continue;
         }
         // Skip if target pixel contains a valid closer point.
         else if (keep_ == KEEP_CLOSEST)
         {
-          auto x_out = x_out_begin + i_out * output.width + j_out;
           if (is_point_valid(x_out[0], x_out[1], x_out[2], false)
               && hypot(x_out[0], x_out[1], x_out[2]) <= hypot(x_in[0], x_in[1], x_in[2]))
             continue;
@@ -122,7 +135,6 @@ public:
         // Skip if target pixel contains a valid farther point.
         else if (keep_ == KEEP_FARTHEST)
         {
-          auto x_out = x_out_begin + i_out * output.width + j_out;
           if (is_point_valid(x_out[0], x_out[1], x_out[2], false)
               && hypot(x_out[0], x_out[1], x_out[2]) >= hypot(x_in[0], x_in[1], x_in[2]))
             continue;
@@ -135,7 +147,6 @@ public:
       }
     }
   }
-
 };
 
 }
--- src/projection_nodelet.cpp
+++ src/projection_nodelet.cpp
@@ -4,6 +4,8 @@
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 namespace cloud_proc
 {
@@ -12,6 +14,10 @@ class ProjectionNodelet : public nodelet::Nodelet
 {
 protected:
     Projection<float> projection_;
+    std::string frame_;
+    double timeout_ = 0.0;
+    tf2_ros::Buffer tf_;
+    std::unique_ptr<tf2_ros::TransformListener> tf_listener_;
     ros::Publisher cloud_pub_;
     ros::Subscriber cloud_sub_;
     ~ProjectionNodelet() override = default;
@@ -20,12 +26,28 @@ public:
     {
       getPrivateNodeHandle().param("height", projection_.height_, projection_.height_);
       getPrivateNodeHandle().param("width", projection_.width_, projection_.width_);
+      getPrivateNodeHandle().param("fov_azimuth", projection_.fov_azimuth_, projection_.fov_azimuth_);
+      getPrivateNodeHandle().param("fov_elevation", projection_.fov_elevation_, projection_.fov_elevation_);
       getPrivateNodeHandle().param("f_azimuth", projection_.f_azimuth_, projection_.f_azimuth_);
       getPrivateNodeHandle().param("f_elevation", projection_.f_elevation_, projection_.f_elevation_);
       getPrivateNodeHandle().param("c_azimuth", projection_.c_azimuth_, projection_.c_azimuth_);
       getPrivateNodeHandle().param("c_elevation", projection_.c_elevation_, projection_.c_elevation_);
       getPrivateNodeHandle().param("keep", projection_.keep_, projection_.keep_);
       getPrivateNodeHandle().param("azimuth_only", projection_.azimuth_only_, projection_.azimuth_only_);
+      getPrivateNodeHandle().param("frame", frame_, frame_);
+      getPrivateNodeHandle().param("timeout", timeout_, timeout_);
+      NODELET_INFO("Height: %i", projection_.height_);
+      NODELET_INFO("Width: %i", projection_.width_);
+      NODELET_INFO("Field of view in azimuth: %f", projection_.fov_azimuth_);
+      NODELET_INFO("Field of view in elevation: %f", projection_.fov_elevation_);
+      NODELET_INFO("Focal length azimuth: %f", projection_.f_azimuth_);
+      NODELET_INFO("Focal length elevation: %f", projection_.f_elevation_);
+      NODELET_INFO("Principal point azimuth: %f", projection_.c_azimuth_);
+      NODELET_INFO("Principal point elevation: %f", projection_.c_elevation_);
+      NODELET_INFO("Keep: %i", projection_.keep_);
+      NODELET_INFO("Azimuth only: %i", projection_.azimuth_only_);
+      NODELET_INFO("Use rotation to frame: %s", frame_.c_str());
+      NODELET_INFO("Timeout: %.3f s", timeout_);
     }
     void advertise()
     {
@@ -33,6 +55,8 @@ public:
     }
     void subscribe()
     {
+      if (!frame_.empty())
+        tf_listener_ = std::make_unique<tf2_ros::TransformListener>(tf_);
       cloud_sub_ = getNodeHandle().subscribe("input", 2, &ProjectionNodelet::onCloud, this);
     }
     void onInit() override
@@ -44,8 +68,27 @@ public:
     void onCloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
     {
       Timer t;
+      geometry_msgs::TransformStamped tf;
+      tf.transform.rotation.w = 1.0;
+      if (!frame_.empty())
+      {
+        try
+        {
+          tf = tf_.lookupTransform(frame_, msg->header.frame_id, msg->header.stamp, ros::Duration(timeout_));
+          tf.transform.translation.x = 0;
+          tf.transform.translation.y = 0;
+          tf.transform.translation.z = 0;
+        }
+        catch (tf2::TransformException& ex)
+        {
+          NODELET_ERROR("Could not transform %s to %s: %s.", msg->header.frame_id.c_str(), frame_.c_str(), ex.what());
+          return;
+        }
+        NODELET_INFO("Waited for transform: %f s.", t.secondsElapsed());
+      }
+      t.reset();
       auto output = boost::make_shared<sensor_msgs::PointCloud2>();
-      projection_.process(*msg, *output);
+      projection_.process(*msg, tf.transform, *output);
       cloud_pub_.publish(output);
       NODELET_INFO("Projected %lu points: %f s.", num_points(*msg), t.secondsElapsed());
     }
